c5c0dfe8219a38ccf7285c628446e6e1e65a438f
Michael Martin <3277009+flrgh@users.noreply.github.com>
2019-06-20 11:59:53 -0700

    formatting

--- install.sh
+++ install.sh
@@ -5,19 +5,22 @@ DOTFILES_DIRECTORY="$PWD"
 mkdir -p "$HOME/.dotfiles.old"
 
 for item in .vim .vimrc .tmux.conf .bash_profile .bashrc; do
-    if [ -L "$HOME/$item" ] ; then
+    if [[ -L "$HOME/$item" ]]; then
         rm "$HOME/$item"
-    elif [ -f "$HOME/$item" -o -d "$HOME/$item" ] ; then
+    elif [[ -f "$HOME/$item" || -d "$HOME/$item" ]]; then
         mv "$HOME/$item" "$HOME/.dotfiles.old/"
     fi
     ln -s "$DOTFILES_DIRECTORY/$item" "$HOME/$item"
 done
 
-mkdir -vp $HOME/.config/nvim
-cp -v init.vim $HOME/.config/nvim/
+mkdir -vp "$HOME/.config/nvim"
+cp -v init.vim "$HOME/.config/nvim/"
 
 # Set up Vundle for vim
 git submodule update --init
-vim --not-a-term +BundleInstall +qall
 
+echo "Installing vim plugins"
+\vim --not-a-term +BundleInstall +qall
+
+echo "Installing neovim plugins"
 nvim --headless +PlugUpdate +qall
