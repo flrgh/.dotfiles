d81d66b2834f970fb187d3781de5a03ec4066bc7
Michael Martin <3277009+flrgh@users.noreply.github.com>
2019-08-13 16:42:47 -0700

    Add ipython config

--- dirs.d/.bash/20-python-startup.sh
+++ dirs.d/.bash/20-python-startup.sh
@@ -2,3 +2,5 @@ PYTHONSTARTUP=$HOME/.local/.startup.py
 if [[ -f $PYTHONSTARTUP ]]; then
     export PYTHONSTARTUP
 fi
+
+export IPYTHONDIR="$HOME/.config/ipython"
---
+++ files.d/.config/ipython/profile_default/ipython_config.py
@@ -0,0 +1,1 @@
+get_config().TerminalInteractiveShell.confirm_exit = False
