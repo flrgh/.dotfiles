d18310dabcec7490e3599518d9c66c9859a230cf
Michael Martin <3277009+flrgh@users.noreply.github.com>
2019-08-14 15:27:30 -0700

    Add tab-completion generator post hook

---
+++ hooks.post/init-bash-completion.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eu
+
+COMPLETE=(
+    "pip completion --bash"
+    "pip3 completion --bash"
+    "hugo gen autocomplete --completionfile=%%FILE%%"
+)
+
+DIR="$HOME/.local/.bash_completion.d"
+
+echo "Initializing bash completion"
+
+for cmd in "${COMPLETE[@]}"; do
+    bin=${cmd%% *}
+    if command -v "$bin" &>/dev/null; then
+        echo "$bin"
+        out="${DIR}/$bin"
+        if [[ $cmd == *%%FILE%%* ]]; then
+            ${cmd//%%FILE%%/$out}
+        else
+            $cmd > "$DIR/$bin"
+        fi
+    else
+        echo "$bin (skipped)"
+    fi
+done
