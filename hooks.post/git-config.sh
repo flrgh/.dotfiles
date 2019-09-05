b9d40331caa12ea6378a775c7bd037f03eb95262
Michael Martin <3277009+flrgh@users.noreply.github.com>
2019-09-05 09:40:55 -0700

    trying out tpope's git tag config https://tbaggery.com/2011/08/08/effortless-ctags-with-git.html

---
+++ hooks.post/git-config.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -eu
+
+CONF="$REPO_ROOT/assets/git-config"
+
+INSTALL_DIR=$INSTALL_PATH/.config/git
+
+ls_items() {
+    git config \
+        --file "$CONF" \
+        --list \
+        --name-only
+}
+
+set_item() {
+    local -r name=$1
+    echo "configurating $name"
+    local val
+    val=$(git config --file "$CONF" --get "$name")
+
+    git config \
+        --file "$INSTALL_DIR/config" \
+        "$name" "$val"
+}
+
+mkdir -p "$INSTALL_DIR"
+
+for item in $(ls_items); do
+    set_item "$item"
+done
