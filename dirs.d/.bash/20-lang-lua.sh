b6a2c399b0b9b6a2ec81132570b26a8a3d93f11e
Michael Martin <3277009+flrgh@users.noreply.github.com>
2020-06-20 12:06:33 -0700

    Fix luarocks path madness

--- dirs.d/.bash/20-lang-lua.sh
+++ dirs.d/.bash/20-lang-lua.sh
@@ -16,16 +16,13 @@ if iHave luarocks; then
         if (( mtime + (24*60*60) < _now )); then
             _debug_rc "luarocks path cache is stale--rebuilding"
             rm "$file"
-            luarocks path --lr-path > "$file"
-            luarocks path --lr-cpath >> "$file"
+            luarocks path --no-bin > "$file"
         fi
 
     else
         _debug_rc "Creating luarocks path cache"
-        luarocks path --lr-path > "$file"
-        luarocks path --lr-cpath >> "$file"
+        luarocks path --no-bin > "$file"
     fi
 
-    addPath "$(head -1 "$file")" "LUA_PATH" ";"
-    addPath "$(tail -1 "$file")" "LUA_CPATH" ";"
+    . "$file"
 fi
