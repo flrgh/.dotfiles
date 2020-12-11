4921587b4211b24723113da95c68db53b06088ef
Michael Martin <3277009+flrgh@users.noreply.github.com>
2020-12-11 11:44:00 -0800

    bash: use bat for a man pager if it exists

--- dirs.d/.bash/10-config.sh
+++ dirs.d/.bash/10-config.sh
@@ -12,3 +12,9 @@ shopt -s checkwinsize
 
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+# use bat as a man pager if it exists
+if iHave bat; then
+    export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+    export MANROFFOPT="-c"
+fi
