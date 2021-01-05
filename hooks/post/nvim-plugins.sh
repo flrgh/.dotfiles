059650e8d6203daf4340b9bf0e608fc52a34fec3
Michael Martin <3277009+flrgh@users.noreply.github.com>
2021-01-04 19:32:27 -0800

    hooks: install/update vim-plug

--- hooks/post/nvim-plugins.sh
+++ hooks/post/nvim-plugins.sh
@@ -7,5 +7,13 @@ set -eu
 echo "Installing python deps for neovim"
 pip3 install --user --upgrade pynvim
 
+echo "Installing/updating vim-plug"
+curl -f \
+    -s \
+    -L \
+    -o "$HOME/.config/nvim/autoload/plug.vim" \
+    --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 echo "Installing neovim plugins"
 nvim --headless +PlugUpdate +qall
