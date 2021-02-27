ce337e50998746207c0d31c22c7c3b09eec49aff
Michael Martin <3277009+flrgh@users.noreply.github.com>
2021-02-26 16:26:29 -0800

    fzf: update default options

--- dirs.d/.bash/09-finder.sh
+++ dirs.d/.bash/09-finder.sh
@@ -1,12 +1,14 @@
 # options for fzf
 if iHave fzf; then
-    if iHave fd; then
-        export FZF_DEFAULT_COMMAND='fd --type f --color=never'
-        export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-        export FZF_ALT_C_COMMAND='fd --type d . --color=never'
-    fi
-
     if [[ -f /usr/share/fzf/shell/key-bindings.bash ]]; then
         . /usr/share/fzf/shell/key-bindings.bash
+    fi
+
+    FZF_DEFAULT_OPTS="--info=default --height=80% --border=sharp --tabstop=4"
+
+    if iHave fd; then
+        export FZF_DEFAULT_COMMAND='fd --type f --color=never'
+        #export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+        #export FZF_ALT_C_COMMAND='fd --type d . --color=never'
     fi
 fi
