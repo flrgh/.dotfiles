98cb23f124ba32b3fd5d763e3591eb6cbabc28ac
Michael Martin <3277009+flrgh@users.noreply.github.com>
2021-07-03 12:06:16 -0700

    nvim: fix command path for json language server

--- files.d/.config/nvim/lua/lsp/json.lua
+++ files.d/.config/nvim/lua/lsp/json.lua
@@ -1,6 +1,7 @@
 return function(on_attach, lsp, _)
     if vim.fn.executable("vscode-json-languageserver") then
         lsp.jsonls.setup {
+            cmd = {"vscode-json-languageserver"},
             filetypes = {"json"},
             on_attach = on_attach
         }
