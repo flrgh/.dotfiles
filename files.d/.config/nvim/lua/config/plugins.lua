7ae659e2304ded807d6ee01513aa2de7de6c438b
Michael Martin <3277009+flrgh@users.noreply.github.com>
2021-10-14 13:54:29 -0700

    neovim: move telescope/fzf configs around

--- files.d/.config/nvim/lua/config/plugins.lua
+++ files.d/.config/nvim/lua/config/plugins.lua
@@ -293,7 +293,54 @@ return require('packer').startup(function(use)
 
   use {
     'nvim-telescope/telescope.nvim',
-    requires = { 'nvim-lua/plenary.nvim' }
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local actions = require "telescope.actions"
+      require('telescope').setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
+            },
+          },
+          layout_strategy = "vertical",
+          layout_config = {
+            horizontal = {
+              width = 0.99,
+              height = 0.9,
+              prompt_position = "bottom",
+              preview_cutoff = 120,
+            },
+
+            vertical = {
+              width = 0.9,
+              height = 0.9,
+              prompt_position = "bottom",
+              preview_cutoff = 40,
+            },
+
+            center = {
+              width = 0.9,
+              height = 0.9,
+              preview_cutoff = 40,
+              prompt_position = "top",
+            },
+
+            cursor = {
+              width = 0.9,
+              height = 0.9,
+              preview_cutoff = 40,
+            },
+
+            bottom_pane = {
+              height = 25,
+              prompt_position = "top",
+            },
+          },
+        },
+      })
+    end,
   }
 
   use {
@@ -302,6 +349,23 @@ return require('packer').startup(function(use)
     requires = {
       'nvim-telescope/telescope.nvim',
     },
+    config = function()
+      require('telescope').setup({
+        extensions = {
+          fzf = {
+            fuzzy                   = true,         -- false will only do exact matching
+            override_generic_sorter = false,        -- override the generic sorter
+            override_file_sorter    = true,         -- override the file sorter
+            case_mode               = "smart_case", -- or "ignore_case" or "respect_case"
+                                                    -- the default case_mode is "smart_case"
+          }
+        }
+      })
+
+      -- To get fzf loaded and working with telescope, you need to call
+      -- load_extension, somewhere after setup function:
+      require('telescope').load_extension('fzf')
+    end,
   }
 
   use {
