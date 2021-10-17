1ed0357a975dc7d931d2511e7c964145bc001526
Michael Martin <3277009+flrgh@users.noreply.github.com>
2021-10-17 16:48:19 -0700

    neovim: config code cleanup

--- files.d/.config/nvim/lua/fs.lua
+++ files.d/.config/nvim/lua/fs.lua
@@ -1,52 +1,52 @@
 local _M = {
-    _VERSION = '0.1'
+  _VERSION = '0.1'
 }
 
 local lfs = require 'lfs'
 
 --- Check if a file exists.
--- @tparam fname string
--- @treturn exists boolean
+---@param  fname   string
+---@return boolean exists
 function _M.file_exists(fname)
-    local f = io.open(fname, 'rb')
-    if f then f:close() end
-    return f ~= nil
+  local f = io.open(fname, 'rb')
+  if f then f:close() end
+  return f ~= nil
 end
 
 --- Check if a directory exists.
--- @tparam fname string
--- @treturn exists boolean
+---@param  fname   string
+---@return boolean exists
 function _M.dir_exists(fname)
-    return lfs.attributes(fname, 'mode') == 'directory'
+  return lfs.attributes(fname, 'mode') == 'directory'
 end
 
 --- Read a file's contents to a string.
--- @tparam fname string
--- @treturn content string
--- @treturn err string
+---@param  fname   string
+---@return string? content
+---@return string? error
 function _M.read_file(fname)
-    local f, err = io.open(fname, 'rb')
-    if not f then
-        return nil, err
-    end
+  local f, err = io.open(fname, 'rb')
+  if not f then
+    return nil, err
+  end
 
-    local content = f:read('*all')
-    f:close()
+  local content = f:read('*all')
+  f:close()
 
-    return content
+  return content
 end
 
 --- Decode the contents of a json file.
--- @tparam fname string
--- @return json
--- @treturn err string
+---@param  fname   string
+---@return any?    json
+---@return string? error
 function _M.read_json_file(fname)
-    local raw, err = _M.read_file(fname)
-    if not raw then
-        return nil, err
-    end
+  local raw, err = _M.read_file(fname)
+  if not raw then
+    return nil, err
+  end
 
-    return vim.fn.json_decode(raw)
+  return vim.fn.json_decode(raw)
 end
 
 return _M
