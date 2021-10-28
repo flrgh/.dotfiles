a9979472a8de5d823a9b48f3d0651c0c4903b57d
Michael Martin <3277009+flrgh@users.noreply.github.com>
2021-10-28 16:12:34 -0700

    WIP

--- files.d/.config/nvim/lua/local/lsp/lua.lua
+++ files.d/.config/nvim/lua/local/lsp/lua.lua
@@ -1,7 +1,10 @@
 local fs = require 'local.fs'
+local mod = require 'local.module'
 
+local stdpath = vim.fn.stdpath
 local expand = vim.fn.expand
 local split = vim.fn.split
+local endswith   = vim.endswith
 local insert = table.insert
 
 local EMPTY = {}
@@ -11,15 +14,16 @@ local USER_SETTINGS = expand('~/.config/lua/lua-lsp.json')
 ---@alias local.lsp.filenames string[]
 
 ---@class local.lsp.settings
----@field log_level number
+---@field include_vim boolean
 ---@field third_party? local.lsp.filenames
 local DEFAULT_SETTINGS = {
 
+  -- Make the server aware of Neovim runtime files
+  include_vim = false,
+
   ---@class local.lsp.settings.lib : table
-  ---@field include_vim boolean
   ---@field extra?      local.lsp.filenames
   lib = {
-    include_vim = false, -- Make the server aware of Neovim runtime files
     extra = {},
   },
 
@@ -29,17 +33,27 @@ local DEFAULT_SETTINGS = {
     extra = {},
   },
 
-  log_level = 2,
-
   third_party = nil,
 }
 
 ---@param p string
----@return string[]
+---@return local.lsp.filenames
 local function expand_paths(p)
   p = expand(p)
-  p = split(p, "\n")
+  p = split(p, "\n", false)
   return p
+end
+
+---@return local.lsp.filenames
+local function packer_dirs()
+  local dirs = {}
+  mod.if_exists('packer', function()
+    local glob = stdpath('data') .. '/site/pack/packer/*/*/lua'
+    for _, dir in ipairs(expand_paths(glob)) do
+      insert(dirs, dir)
+    end
+  end)
+  return dirs
 end
 
 ---@param t table
@@ -74,41 +88,63 @@ end
 
 ---@param settings local.lsp.settings
 local function lua_libs(settings)
-  local opts = settings.lib
   local libs = {}
-  if opts.include_vim then
+
+  if settings.include_vim then
     libs[expand('$VIMRUNTIME/lua')] = true
-    libs[expand('$VIMRUNTIME/lua/vim/lsp')] = true
+
+    for _, dir in ipairs(packer_dirs()) do
+      libs[dir] = true
+    end
   end
 
-  for _, item in ipairs(opts.extra) do
+  for _, item in ipairs(settings.lib.extra or EMPTY) do
     for _, elem in ipairs(expand_paths(item)) do
       libs[elem] = true
     end
   end
 
   libs[expand("$PWD")] = true
-  libs[expand("$PWD/lua")] = true
-  libs[expand("$PWD/src")] = true
+
+  local ws = fs.workspace_root()
+  if ws then
+    libs[ws] = true
+  end
 
   return libs
+end
+
+---@param paths local.lsp.filenames
+---@param dir string
+local function add_lua_path(paths, dir)
+  insert(paths, dir .. '/?.lua')
+  insert(paths, dir .. '/?/init.lua')
 end
 
 ---@param settings local.lsp.settings
 ---@param libs table<string, boolean>
 ---@return string[]
 local function lua_path(settings, libs)
-  local path = vim.split(package.path, ';')
+  local path = split(package.path, ';', false)
 
   for lib in pairs(libs) do
-    insert(path, lib .. '/?.lua')
-    insert(path, lib .. '/?/init.lua')
+    -- add $path
+    add_lua_path(path, lib)
+
+    -- add $path/lua
+    if not endswith(lib, '/lua') and fs.dir_exists(lib .. '/lua') then
+      add_lua_path(path, lib .. '/lua')
+    end
+
+    -- add $path/src
+    if not endswith(lib, '/src') and fs.dir_exists(lib .. '/src') then
+      add_lua_path(path, lib .. '/src')
+    end
   end
 
   for _, extra in ipairs(settings.path.extra or EMPTY) do
     for _, elem in ipairs(expand_paths(extra)) do
-      insert(path, elem .. '/?.lua')
-      insert(path, elem .. '/?/init.lua')
+      add_lua_path(path, elem)
     end
   end
 
