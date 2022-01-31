20f3233ba330a860004593a9441b04997770ce0c
Michael Martin <3277009+flrgh@users.noreply.github.com>
2022-01-30 19:34:58 -0800

    neovim: add env var hooks for turning on debugging

--- files.d/.config/nvim/lua/local/config/globals.lua
+++ files.d/.config/nvim/lua/local/config/globals.lua
@@ -28,3 +28,44 @@ do
     globals.workspace = ws
   end
 end
+
+
+--- LSP debug enabled
+---@type boolean
+globals.lsp_debug = false
+
+do
+  local env = os.getenv("NVIM_LSP_DEBUG")
+  if env and env ~= "0" then
+    globals.lsp_debug = true
+
+    -- vim.log.levels.TRACE will show even more info but has not been all that
+    local level = vim.log.levels[env] or vim.log.levels.DEBUG
+
+    vim.lsp.set_log_level(level)
+
+    -- omit the metatable from vim.inspect outoupt
+    local inspect = vim.inspect
+    local METATABLE = inspect.METATABLE
+    local opts = {
+      process = function(item, path)
+        if path[#path] ~= METATABLE then return item end
+      end
+    }
+
+    require("vim.lsp.log").set_format_func(function(item)
+      return inspect(item, opts)
+    end)
+  end
+end
+
+---@type boolean
+globals.debug = false
+do
+  local env = os.getenv("NVIM_DEBUG")
+  if env and env ~= "0" then
+    globals.debug = true
+  end
+end
+
+return globals
