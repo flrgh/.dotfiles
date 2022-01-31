f054e6c28ea6f4743856ac73cb650f4354940492
Michael Martin <3277009+flrgh@users.noreply.github.com>
2022-01-30 19:28:59 -0800

    hooks: fix nvim plugin update/installer

--- hooks/post/nvim-plugins.sh
+++ hooks/post/nvim-plugins.sh
@@ -11,11 +11,13 @@ echo "Updating neovim"
 "$HOME"/.local/libexec/install/install-neovim latest
 
 echo "Installing neovim plugins"
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerCompile'
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerUpdate'
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
 echo "Installing tree-sitter CLI"
 "$HOME/.local/libexec/install/tools/install-tree-sitter"
+
+echo "Upgrading tree-sitter grammers"
+nvim --headless -c 'TSUpdateSync' -c 'quitall'
 
 echo
 echo "Finished"
