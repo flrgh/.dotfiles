75c597c27b856d8c6d3aa0dd65e5166ab79fe319
Michael Martin <3277009+flrgh@users.noreply.github.com>
2022-01-30 19:34:25 -0800

    neovim: try to use lspconfig.util to determine workspace

--- files.d/.config/nvim/lua/local/config/globals.lua
+++ files.d/.config/nvim/lua/local/config/globals.lua
@@ -1,8 +1,30 @@
-do
-  local fs = require 'local.fs'
+local globals = {}
 
-  local ws = fs.workspace_root()
+--- nvim workspace directory
+---@type string|nil
+globals.workspace = nil
+
+do
+  local mod = require "local.module"
+  local ws
+
+  if mod.exists("lspconfig") then
+    local util = require "lspconfig.util"
+    local f = vim.fn.expand("%:p:h")
+    ws = util.find_git_ancestor(f)
+  end
+
+  local fs = require 'local.fs'
+  if not ws then
+    ws = fs.workspace_root()
+  end
+
+  if not ws then
+    ws = fs.buffer_directory()
+  end
+
   if ws then
     vim.fn.setenv("NVIM_WORKSPACE", ws)
+    globals.workspace = ws
   end
 end
