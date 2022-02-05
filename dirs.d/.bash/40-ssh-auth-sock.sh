5c2043becb9e2f469717ec65a60d5aefdffebef1
Michael Martin <3277009+flrgh@users.noreply.github.com>
2022-02-05 13:41:48 -0800

    bash: maybe maybe fix ssh auth sock issue

--- dirs.d/.bash/40-ssh-auth-sock.sh
+++ dirs.d/.bash/40-ssh-auth-sock.sh
@@ -1,5 +1,10 @@
 # Make ssh agent forwarding work with persistent tmux/screen sessions
-if [[ -S "$SSH_AUTH_SOCK" && ! -h "$SSH_AUTH_SOCK" ]]; then
+if test -S "/run/user/$UID/keyring/.ssh"; then
+    ln -sf "/run/user/$UID/keyring/.ssh" ~/.ssh/ssh_auth_sock
+    export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
+
+elif [[ -S "$SSH_AUTH_SOCK" && ! -h "$SSH_AUTH_SOCK" ]]; then
+
     _debug_rc "ln -sf \"$SSH_AUTH_SOCK\" ~/.ssh/ssh_auth_sock"
     ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
 fi
