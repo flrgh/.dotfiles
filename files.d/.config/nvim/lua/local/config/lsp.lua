bf0d4e209f677ca3c10a56162eb4d945ebd3ba50
Michael Martin <3277009+flrgh@users.noreply.github.com>
2022-02-10 11:14:34 -0800

    neovim: always jump to first definition

--- files.d/.config/nvim/lua/local/config/lsp.lua
+++ files.d/.config/nvim/lua/local/config/lsp.lua
@@ -9,6 +9,20 @@ if not mod.exists('lspconfig') then
   return
 end
 
+vim.lsp.handlers["textDocument/definition"] = function(_, result)
+  if not result or vim.tbl_isempty(result) then
+    print "[LSP] Could not find definition"
+    return
+  end
+
+  if vim.tbl_islist(result) then
+    vim.lsp.util.jump_to_location(result[1], "utf-8")
+  else
+    vim.lsp.util.jump_to_location(result, "utf-8")
+  end
+end
+
+
 local lspconfig = require 'lspconfig'
 
 ---@param client table
