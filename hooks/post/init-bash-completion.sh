42a8cfffd4c7febc94c303f4f6b00179a4693dc7
Michael Martin <3277009+flrgh@users.noreply.github.com>
2022-03-22 11:59:21 -0700

    hooks: refactor bash completion installer

--- hooks/post/init-bash-completion.sh
+++ hooks/post/init-bash-completion.sh
@@ -2,45 +2,56 @@
 
 set -eu
 
-COMPLETE=(
-    "pip completion --bash"
-    "pip3 completion --bash"
-    "hugo gen autocomplete --completionfile=%%FILE%%"
-    "openstack complete --shell bash"
-    "kubectl completion bash"
-    "luarocks completion bash"
-    "gh completion --shell bash" # github cli
-    "op completion bash"         # 1password
-)
+have() {
+    command -v "$1" &>/dev/null
+}
 
 DIR="$HOME/.local/.bash_completion.d"
 
+declare -A COMMANDS=(
+    [pip]="completion --bash"
+    [pip3]="completion --bash"
+    [hugo]="gen autocomplete --completionfile=%%FILE%%"
+    [openstack]="complete --shell bash"
+    [kubectl]="completion bash"
+    [luarocks]="completion bash"
+    [gh]="completion --shell bash" # github cli
+    [op]="completion bash"         # 1password
+)
+
+declare -A REMOTE_FILES=(
+    [busted]="https://raw.githubusercontent.com/Olivine-Labs/busted/master/completions/bash/busted.bash"
+)
+
+
 echo "Initializing bash completion"
 
-for cmd in "${COMPLETE[@]}"; do
-    bin=${cmd%% *}
-    if command -v "$bin" &>/dev/null; then
-        echo "$bin"
+for bin in "${!COMMANDS[@]}"; do
+    if have "$bin"; then
+        cmd="$bin ${COMMANDS[$bin]}"
+
+        echo "Generating $bin completion from '$cmd'"
+
         out="${DIR}/$bin"
+
         if [[ $cmd == *%%FILE%%* ]]; then
             ${cmd//%%FILE%%/$out}
         else
             $cmd > "$DIR/$bin"
         fi
     else
-        echo "$bin (skipped)"
+        echo "Skipping $bin"
     fi
 done
 
-declare -A REMOTE_FILES=(
-    [busted]="https://raw.githubusercontent.com/Olivine-Labs/busted/master/completions/bash/busted.bash"
-)
-
 for bin in "${!REMOTE_FILES[@]}"; do
-    url=${REMOTE_FILES[$bin]}
-    if command -v "$bin" &>/dev/null; then
+    if have "$bin"; then
+        url=${REMOTE_FILES[$bin]}
+
         echo "Downloading $bin completion from $url"
         f=$(cache-get "$url" "bash-completion-${bin}")
         cat "$f" > "$DIR/$bin"
+    else
+        echo "Skipping $bin"
     fi
 done
