843a5f7e8196cf3ee2a2d17e290c27f716ebd306
Michael Martin <3277009+flrgh@users.noreply.github.com>
2022-05-21 12:52:45 -0700

    installer: create some common dirs during install

---
+++ hooks/pre/mkdirs.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+INSTALL_PATH=${2:-$HOME}
+
+DIRS=(
+    .cache
+    .cache/download
+    .config
+    .local
+    .local/bin
+    .local/include
+    .local/lib
+    .local/libexec
+    .local/share
+    .local/var
+    .local/var/log
+    .local/var/log/nvim
+    .local/var/log/lsp
+)
+
+for d in "${DIRS[@]}"; do
+    mkdir -v -p "$INSTALL_PATH/$d"
+done
