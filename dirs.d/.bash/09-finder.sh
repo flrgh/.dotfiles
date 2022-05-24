629f027ebce25c0f03b2e9056de0d3fb581f7bb5
Michael Martin <3277009+flrgh@users.noreply.github.com>
2022-05-24 10:47:35 -0700

    bash: add alias/helper for fd

--- dirs.d/.bash/09-finder.sh
+++ dirs.d/.bash/09-finder.sh
@@ -1,6 +1,6 @@
 # options for fzf
 if iHave fzf; then
-    FZF_DEFAULT_OPTS="--info=default --height=80% --border=sharp --tabstop=4"
+    export FZF_DEFAULT_OPTS="--info=default --height=80% --border=sharp --tabstop=4"
 
     if iHave fd; then
         export FZF_DEFAULT_COMMAND='fd --hidden --type f --color=never'
@@ -21,4 +21,34 @@ if iHave fzf; then
     if [[ -f /usr/share/fzf/shell/key-bindings.bash ]]; then
         _source_file /usr/share/fzf/shell/key-bindings.bash
     fi
+fi
+
+
+# I have `find /some/path` permanently stored in muscle memory. This is an
+# issue as I try using fd as a replacement, because the same invocation yields
+# a "you're doing it wrong!" error:
+#
+#
+# > [fd error]: The search pattern './' contains a path-separation character ('/') and will not lead to any search results.
+# >
+# > If you want to search for all files inside the './' directory, use a match-all pattern:
+# >
+# >  fd . './'
+# >
+# > If nstead, if you want your pattern to match the full file path, use:
+# >
+# >  fd --full-path './'
+#
+#
+# This little function exists to handle that case so that I don't have to
+# relearn anything
+#
+if iHave fd; then
+    fd() {
+        if (( $# == 1 )) && [[ $1 != . ]]; then
+            command fd . "$1"
+        else
+            command fd "$@"
+        fi
+    }
 fi
