b079aae17e240491e50034ec0dcd6115724187c4
Michael Martin <3277009+flrgh@users.noreply.github.com>
2023-03-14 16:05:30 -0700

    cargo: update config

--- home/.local/bash/rc.d/20-lang-rust.sh
+++ home/.local/bash/rc.d/20-lang-rust.sh
@@ -1,6 +1,9 @@
 export CARGO_HOME=$HOME/.local/cargo
 addPath "$CARGO_HOME"/bin
 
+# https://blog.rust-lang.org/2023/03/09/Rust-1.68.0.html#cargos-sparse-protocol
+export CARGO_REGISTRIES_CRATES_IO_PROTOCOL=sparse
+
 export RUSTUP_HOME=$HOME/.local/rustup
 if [[ -d "$RUSTUP_HOME"/bin ]]; then
     addPath "$RUSTUP_HOME"/bin
