a87f83e5803d054417216efaf792e70086d75ce4
Michael Martin <3277009+flrgh@users.noreply.github.com>
2023-10-06 11:18:05 -0700

    lua: rewrite repl pretty print

--- home/.config/lua/repl.lua
+++ home/.config/lua/repl.lua
@@ -1,68 +1,52 @@
 --- lua repl init
-
 do
-  local function import(mod)
-    local ok, imported = pcall(require, mod)
+  local pcall = pcall
+  local require = require
+
+  local ok, term = pcall(require, "term")
+
+  if ok and term.isatty(io.stdout) then
+    local pretty
+    ok, pretty = pcall(require, "pl.pretty")
+
     if ok then
-      return imported
+      local write = pretty.write
+
+      local fmt = string.format
+      local rep = string.rep
+
+      local select = select
+      local tostring = tostring
+      local type = type
+
+      local function _tostring(obj)
+        if type(obj) == 'table' then
+          return write(obj)
+        end
+        return tostring(obj)
+      end
+
+      local function _map(func, n, ...)
+        if n == 0 then return end
+
+        local first = select(1, ...)
+
+        if n == 1 then
+          return func(first)
+        end
+
+        return func(first), _map(func, select(2, ...))
+      end
+
+      _G.old_print = print
+      local old_print = _G.old_print
+      local s = "%s"
+      local space = " "
+      function _G.print(...)
+        local n = select("#", ...)
+        local tmpl = rep(s, n, space)
+        return old_print(fmt(tmpl, _map(_tostring, n, ...)))
+      end
     end
   end
-
-  local function interactive()
-    local term = import('term')
-
-    if not term then
-      return false, 'no lua-term'
-    end
-
-    return term.isatty(io.stdout)
-  end
-
-  if not interactive() then
-    -- we're not in an interactive session, so let's bail on the whole thing
-    return
-  end
-
-  local _pl = import('pl.import_into')
-  if _pl then
-    _G.pl = _pl()
-  end
-end
-
-do
-  if not _G.pl then
-    print("penlight isn't installed :( no fancy print for you")
-    return
-  end
-
-  local pl = _G.pl
-  local write = pl.pretty.write
-  local function _tostring(obj)
-    if type(obj) == 'table' then
-      return write(obj)
-    end
-    return tostring(obj)
-  end
-
-  local function _map(func, ...)
-    local n = select('#', ...)
-    if n == 0 then return end
-    local first = select(1, ...)
-    if n == 1 then
-      return func(first)
-    end
-    return func(first), _map(func, select(2, ...))
-  end
-
-  local print = print
-  local s = '%s'
-  function _G._print(...)
-    local fmt = s:rep(select("#", ...), ' ')
-    return print(fmt:format(_map(_tostring,...)))
-  end
-end
-
-if _print then
-  _G.old_print = print
-  _G.print = _print
 end
