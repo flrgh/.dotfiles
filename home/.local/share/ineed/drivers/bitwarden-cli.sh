31ef4982a929d1388e2354f6e56b29740dba7bd7
Michael Martin <3277009+flrgh@users.noreply.github.com>
2024-05-23 11:58:26 -0700

    ineed: convert bitwarden cli script to driver

---
+++ home/.local/share/ineed/drivers/bitwarden-cli.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+readonly REPO=bitwarden/clients
+readonly BASE_URL=https://github.com/bitwarden/clients/releases/download
+readonly BIN=bw
+
+is-installed() {
+    binary-exists "$BIN"
+}
+
+get-installed-version() {
+    if is-installed; then
+        "$BIN" --version
+    fi
+}
+
+get-latest-version() {
+    gh-helper get-releases "$REPO" \
+        | jq -r '.[].tag_name | select(test("^cli"))' \
+        | sed -re 's/^cli-v//' \
+        | sort --version-sort --reverse \
+        | head -1
+}
+
+get-asset-download-url() {
+    local -r version=$1
+    # https://github.com/bitwarden/clients/releases/download/cli-v2024.4.1/bw-linux-2024.4.1.zip
+    echo "${BASE_URL}/cli-v${version}/bw-linux-${version}.zip"
+}
+
+install-from-asset() {
+    local -r asset=$1
+    local -r version=$2
+
+    echo "Fetching checksum for bitwarden $version"
+    # https://github.com/bitwarden/clients/releases/download/cli-v2024.4.1/bw-linux-sha256-2024.4.1.txt
+    local sum_file
+    sum_file=$(cache-get "${BASE_URL}/cli-v${version}/bw-linux-sha256-${version}.txt")
+    local checksum
+    checksum=$(tr -d $'\r' < "$sum_file")
+
+    echo "Validating download checksum"
+
+    printf "%q %q\n" "$checksum" "$asset" \
+        | sha256sum --check --strict
+
+    local tmp; tmp=$(mktemp -d)
+    cd "$tmp"
+    unzip "$asset"
+    ls -la "$BIN"
+    chmod +x "$BIN"
+    mv -v "$BIN" ~/.local/bin/
+}
