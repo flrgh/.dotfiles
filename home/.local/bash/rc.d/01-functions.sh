3209161d3d25e73f0a00f489c37fd5756394d27a
Michael Martin <3277009+flrgh@users.noreply.github.com>
2024-06-16 13:01:35 -0700

    bash: add vim exit function

--- home/.local/bash/rc.d/01-functions.sh
+++ home/.local/bash/rc.d/01-functions.sh
@@ -3,6 +3,11 @@
 #
 # Functions used only while sourcing .bashrc should go in .bashrc
 
+:q() {
+    echo "hey you're not in vim anymore, but I can exit the shell for you..."
+    sleep 0.75 && exit
+}
+
 extract() {
     if [[ -z $1 ]]; then
         echo "usage: extract <filename>"
