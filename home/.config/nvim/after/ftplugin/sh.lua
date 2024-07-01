9dd66eddb8435ab3747463e55d6236d9277bb683
Michael Martin <3277009+flrgh@users.noreply.github.com>
2024-06-30 20:05:37 -0700

    neovim: check modifiable before changing buffer-y things

--- home/.config/nvim/after/ftplugin/sh.lua
+++ home/.config/nvim/after/ftplugin/sh.lua
@@ -4,4 +4,6 @@ local opt_local = vim.opt_local
 opt_local.iskeyword:append("$")
 
 -- force utf-8
-opt_local.fileencoding = "utf-8"
+if vim.bo.modifiable then
+  opt_local.fileencoding = "utf-8"
+end
