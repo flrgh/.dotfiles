8672ef03a24cae7fd4b1a03b7a836ec138aa7e13
Michael Martin <flrgh@protonmail.com>
2024-11-23 13:20:58 -0800

    bash: add bin-path() user function

--- home/.local/bash/rc.d/01-functions.sh
+++ home/.local/bash/rc.d/01-functions.sh
@@ -319,3 +319,14 @@ dump-matching() {
         fi
     done
 }
+
+bin-path() {
+    local -r name=${1?binary name is required}
+    local path; path=$(builtin type -P "$name")
+
+    if [[ -z $path ]]; then
+        return 1
+    fi
+
+    realpath "$path"
+}
