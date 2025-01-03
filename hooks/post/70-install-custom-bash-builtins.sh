8793e77613d1dd021dadd8bf0f452494405ff86d
Michael Martin <flrgh@protonmail.com>
2025-01-03 10:46:30 -0800

    hooks: improve bash builtin install script

--- hooks/post/70-install-custom-bash-builtins.sh
+++ hooks/post/70-install-custom-bash-builtins.sh
@@ -4,16 +4,25 @@ set -euo pipefail
 
 readonly INSTALL=$HOME/.local/lib/bash/builtins
 readonly SRC=$HOME/git/flrgh/bash-builtin-extras
+readonly REPO=git@github.com:flrgh/bash-builtins.git
+
+if [[ ! -d $SRC ]]; then
+    git clone "$REPO" "$SRC"
+fi
 
 mkdir -p "$INSTALL"
 
-if [[ -d $SRC ]]; then
-    pushd "$SRC"
-    cargo build --release
-    cargo build --release --package varsplice
-    install \
-        --target-directory "$INSTALL" \
-        ./target/release/libbash_builtin_extras.so \
-        ./target/release/libvarsplice.so
-    popd
-fi
+cargo build \
+    --quiet \
+    --keep-going \
+    --manifest-path "$SRC"/Cargo.toml \
+    --release \
+    --workspace
+
+install \
+    --verbose \
+    --compare \
+    -D \
+    --target-directory "$INSTALL" \
+    "$SRC"/target/release/libbash_builtin_extras.so \
+    "$SRC"/target/release/libvarsplice.so
