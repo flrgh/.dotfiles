c60206570f5efe5512534ada9c22af00aaa2339a
Michael Martin <3277009+flrgh@users.noreply.github.com>
2025-01-08 14:22:23 -0800

    bash: update docker CLI env vars

--- home/.local/bash/rc.d/20-app-docker.sh
+++ home/.local/bash/rc.d/20-app-docker.sh
@@ -1,1 +1,7 @@
+# https://docs.docker.com/reference/cli/docker/
 export DOCKER_CONFIG=$XDG_CONFIG_HOME/docker
+
+# OpenTelemetry (just a placeholder to remind future me to check this out)
+unset DOCKER_CLI_OTEL_EXPORTER_OTLP_ENDPOINT
+
+export DOCKER_SCOUT_CACHE_DIR=$XDG_CACHE_HOME/docker-scout
