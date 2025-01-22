248007a204f207b0bfce42cf614bc3fdb6d22eed
Michael Martin <flrgh@protonmail.com>
2025-01-21 19:37:50 -0800

    bash: fix nvim alias

--- hooks/bashrc/11-alias-vim.sh
+++ hooks/bashrc/11-alias-vim.sh
@@ -2,7 +2,7 @@
 
 source "$REPO_ROOT"/lib/bash/generate.bash
 
-if command -v nvm &>/dev/null; then
+if command -v nvim &>/dev/null; then
     bashrc-alias vim nvim
     bashrc-export-var EDITOR nvim
 fi
