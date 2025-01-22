c3aa5a8fb00b7adf9160f05564601d1d7a13187d
Michael Martin <3277009+flrgh@users.noreply.github.com>
2025-01-21 20:54:50 -0800

    bash: remove some things from PATH

--- bash/rc.d/02-path.sh
+++ bash/rc.d/02-path.sh
@@ -1,5 +1,8 @@
 __rc_add_path --prepend PATH "$HOME/.local/bin"
 
+__rc_rm_path PATH /usr/share/Modules/bin
+__rc_rm_path PATH "$HOME/.composer/vendor/bin"
+
 __rc_add_path --prepend MANPATH "$HOME/.local/man"
 __rc_add_path --prepend MANPATH "$HOME/.local/share/man"
 __rc_add_path --append  MANPATH /usr/local/share/man
