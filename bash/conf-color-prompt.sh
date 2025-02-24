c3f12cff6824a41632fc68855fccd2f3b01c6e1a
Michael Martin <flrgh@protonmail.com>
2025-02-23 20:43:30 -0800

    bash: PS1 shouldn't be exported

--- bash/conf-color-prompt.sh
+++ bash/conf-color-prompt.sh
@@ -17,7 +17,7 @@ else
 fi
 
 readonly __ps1_default="${__ps1_prefix} ${__ps1_suffix}"
-export PS1="$__ps1_default"
+PS1="$__ps1_default"
 
 readonly __prompt_cmd="\#"
 declare -gi __need_prompt_reset=0
@@ -25,7 +25,7 @@ declare -gi __last_cmd_number=0
 
 # update PS1 when a command returns a non-zero exit code
 __last_status() {
-    local -i exit_code=$?
+    local -i ec=$?
 
     # we check the command counter so that we can only consider the exit code
     # from "new" commands and reset the state otherwise
@@ -36,15 +36,15 @@ __last_status() {
     local -i last_cmd=$__last_cmd_number
     __last_cmd_number=$counter
 
-    if (( counter > last_cmd && exit_code != 0 )); then
+    if (( counter > last_cmd && ec != 0 )); then
         # uh oh red alert!!!!
         __need_prompt_reset=1
-        export PS1="${__ps1_prefix} (${__prompt_alert}${exit_code}${__prompt_reset}) ${__ps1_suffix}"
+        PS1="${__ps1_prefix} (${__prompt_alert}${ec}${__prompt_reset}) ${__ps1_suffix}"
 
     elif (( __need_prompt_reset == 1 )); then
-        export PS1="$__ps1_default"
+        PS1="$__ps1_default"
         __need_prompt_reset=0
     fi
 
-    return "$exit_code"
+    return "$ec"
 }
