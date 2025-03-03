da76157fe07c0bcf389b9ea30488288d3b741d33
Michael Martin <flrgh@protonmail.com>
2025-03-03 10:44:35 -0800

    man: remove older ~/.local/man directory

--- hooks/bashrc/40-main-includes.sh
+++ hooks/bashrc/40-main-includes.sh
@@ -31,17 +31,36 @@ rc-export BASH_USER_LIB "${HOME:?}/.local/lib/bash"
     rc-workfile-close
 }
 
-# common path tweaks
+# PATH
 {
     rc-add-path --prepend PATH "$HOME/.local/bin"
-
     rc-rm-path PATH /usr/share/Modules/bin
     rc-rm-path PATH "$HOME/.composer/vendor/bin"
+}
 
-    rc-add-path --prepend MANPATH "$HOME/.local/man"
-    rc-add-path --prepend MANPATH "$HOME/.local/share/man"
-    rc-add-path --append  MANPATH /usr/local/share/man
-    rc-add-path --append  MANPATH /usr/share/man
+# MANPATH
+{
+    man_paths=(
+        "$HOME/.local/share/man"
+        "$HOME/.local/man"
+    )
+
+    for path in "${man_paths[@]}"; do
+        [[ -e $path ]] || continue
+        rc-add-path --prepend MANPATH "$path"
+    done
+
+    if [[ -e /etc/man_db.conf ]]; then
+        awk '/^MANPATH_MAP/ {print $3}' /etc/man_db.conf \
+            | sort -u \
+            | while read -r dir; do
+                [[ -e $dir ]] || continue
+                rc-add-path --append  MANPATH "$dir"
+            done
+    else
+        rc-add-path --append  MANPATH /usr/local/share/man
+        rc-add-path --append  MANPATH /usr/share/man
+    fi
 }
 
 {
