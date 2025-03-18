90fd4a46282e8dea57f1d2ac05ce4738cae924ab
Michael Martin <3277009+flrgh@users.noreply.github.com>
2025-03-18 16:21:35 -0700

    ssh: try multiple auth sock paths

--- bash/app-ssh.sh
+++ bash/app-ssh.sh
@@ -1,7 +1,10 @@
 # Make ssh agent forwarding work with persistent tmux/screen sessions
 
-sock=${XDG_RUNTIME_DIR:-/run/user/${UID:-$(id -u)}}/keyring/.ssh
-if [[ ${SSH_AUTH_SOCK:-} != "$sock" ]]; then
-    export SSH_AUTH_SOCK=$sock
-fi
-unset sock
+dir=${XDG_RUNTIME_DIR:-/run/user/${UID:-$(id -u)}}/keyring
+for sock in "${dir}/ssh" "${dir}/.ssh"; do
+    if [[ -e $sock ]]; then
+        export SSH_AUTH_SOCK=$sock
+        break
+    fi
+done
+unset dir sock
