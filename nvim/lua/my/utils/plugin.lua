dd9db5ff052f805618e2a220723dd0d3eff81bf8
Michael Martin <flrgh@protonmail.com>
2025-08-02 12:03:25 -0700

    neovim: fix plugin name indexing

--- nvim/lua/my/utils/plugin.lua
+++ nvim/lua/my/utils/plugin.lua
@@ -9,6 +9,56 @@ local by_name
 ---@type LazyPlugin[]
 local list
 
+local function _index()
+  if list then return end
+
+  by_name = {}
+  list = {}
+
+  ---@param name string
+  ---@param p LazyPlugin
+  local function add_name(name, p)
+    local current = by_name[name]
+    if current and current ~= p then
+      error("name collision for plugins " .. p.name .. " and " .. current.name)
+    end
+    by_name[name] = p
+  end
+
+  ---@param name string
+  ---@param p LazyPlugin
+  local function add(name, p)
+    assert(type(name) == "string")
+    assert(type(p) == "table")
+
+    add_name(name, p)
+    add_name(name:lower(), p)
+
+    if name:find("%.nvim$") then
+      add(name:sub(1, -6), p)
+
+    elseif name:find("%.vim$") then
+      add(name:sub(1, -5), p)
+    end
+  end
+
+  for _, p in ipairs(lazy.plugins()) do
+    -- index by name
+    add(p.name, p)
+
+    if p.url then
+      -- e.g. https://github.com/b0o/schemastore.nvim.git
+      local user, name = p.url:match("github%.com/([^/]+)/(.+)(%.git)$")
+      if user then
+        -- index by {{ github.user }}/{{ github.repo }}
+        add(user .. "/" .. name, p)
+      end
+    end
+
+    table.insert(list, p)
+  end
+end
+
 ---@param name string
 ---@return LazyPlugin? plugin
 ---@return string? error
@@ -17,27 +67,7 @@ function _M.get(name)
     return nil, "lazy.nvim is not loaded"
   end
 
-  if not by_name then
-    by_name = {}
-    list = {}
-
-    for _, p in ipairs(lazy.plugins()) do
-      -- index by name
-      by_name[p.name] = p
-
-      -- index by lowercase name
-      local name_lc = p.name:lower()
-      by_name[name_lc] = by_name[name_lc] or p
-
-      -- index by {{ github.user }}/{{ github.repo }}
-      local repo = p[1]
-      if type(repo) == "string" then
-        by_name[repo] = p
-      end
-
-      table.insert(list, p)
-    end
-  end
+  _index()
 
   local plugin = by_name[name] or by_name[name:lower()]
   if plugin then
@@ -58,6 +88,8 @@ function _M.list()
   if not lazy then
     return {}
   end
+
+  _index()
 
   return list
 end
