89fbd4bd887e5f9f832786e45cf43d84e70cc598
Michael Martin <3277009+flrgh@users.noreply.github.com>
2025-08-26 14:13:10 -0700

    bash: resolve mise shim in bin-path()

--- home/.local/lib/bash/functions/bin-path.bash
+++ home/.local/lib/bash/functions/bin-path.bash
@@ -2,9 +2,19 @@ bin-path() {
     local -r name=${1?binary name is required}
     local path; path=$(builtin type -P "$name")
 
-    if [[ -z $path ]]; then
+    if [[ -z ${path:-} ]]; then
         return 1
     fi
 
+    local -r mise=$HOME/.local/bin/mise
+    local -r shim=$HOME/.local/share/mise/shims/${name}
+
+    # resolve mise shim
+    if [[ $name != "mise" && $path == "$shim" && -x $mise ]]; then
+        local which; which=$("$mise" which "$name")
+        path=${which:-"$path"}
+    fi
+
+    # `mise which $name` might still return a symlink
     command realpath "$path"
 }
