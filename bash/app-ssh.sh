4afe7ef84cb9efbff66fa8cb78161cd9b0df0723
Michael Martin <flrgh@protonmail.com>
2026-05-21 15:40:58 -0700

    fix(ssh): post fedora upgrade things

--- bash/app-ssh.sh
+++ bash/app-ssh.sh
@@ -1,10 +1,14 @@
 # Make ssh agent forwarding work with persistent tmux/screen sessions,
-# using gnome-keyring (workstation) and ssh-agent-switcher (server)
+# using gcr-ssh-agent / gnome-keyring (workstation) and ssh-agent-switcher (server)
 
-__keyring_dir=${XDG_RUNTIME_DIR:-/run/user/${UID:-$(id -u)}}/keyring
-__switcher_sock=${XDG_RUNTIME_DIR:-/run/user/${UID:-$(id -u)}}/ssh-agent.sock
+__runtime_dir=${XDG_RUNTIME_DIR:-/run/user/${UID:-$(id -u)}}
+__gcr_sock=${__runtime_dir}/gcr/ssh
+__keyring_dir=${__runtime_dir}/keyring
+__switcher_sock=${__runtime_dir}/ssh-agent.sock
 
-if [[ -S ${__keyring_dir}/ssh ]]; then
+if [[ -S $__gcr_sock ]]; then
+    export SSH_AUTH_SOCK=$__gcr_sock
+elif [[ -S ${__keyring_dir}/ssh ]]; then
     export SSH_AUTH_SOCK=${__keyring_dir}/ssh
 elif [[ -S ${__keyring_dir}/.ssh ]]; then
     export SSH_AUTH_SOCK=${__keyring_dir}/.ssh
@@ -12,4 +16,4 @@ elif [[ -S $__switcher_sock ]]; then
     export SSH_AUTH_SOCK=$__switcher_sock
 fi
 
-unset __keyring_dir __switcher_sock
+unset __runtime_dir __gcr_sock __keyring_dir __switcher_sock
