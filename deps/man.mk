73c8cee4b6bd9954c0d1bc5548f4fe66387e9be5
Michael Martin <3277009+flrgh@users.noreply.github.com>
2026-06-18 08:53:11 -0700

    man: wire up man page generation for promtool, lsd, and usage

--- deps/man.mk
+++ deps/man.mk
@@ -1,5 +1,6 @@
 $(eval $(call man_command,fzf,MANOPT='-R' $$(MISE) exec fzf -- fzf --man,fzf))
 $(eval $(call man_command,http,http --generate man,http))
+$(eval $(call man_command,promtool,$$(MISE) exec promtool -- promtool --help-man,promtool))
 $(eval $(call man_command,rg,$$(MISE) exec ripgrep -- rg --generate man,ripgrep))
 $(eval $(call man_command,tea,$$(MISE) exec tea -- tea man,tea))
 $(eval $(call man_command,xh,$$(MISE) exec xh -- xh --generate man,xh))
@@ -7,6 +8,8 @@ $(eval $(call man_command,xh,$$(MISE) exec xh -- xh --generate man,xh))
 $(eval $(call man_file,bat,$$(dir $$(shell $$(MISE) which bat))bat.1,bat))
 $(eval $(call man_file,fd,$$(shell $$(MISE) which fd).1,fd))
 $(eval $(call man_file,git-cliff,$$(wildcard $$(shell $$(MISE) where git-cliff)/*/man/git-cliff.1),git-cliff))
+$(eval $(call man_file,lsd,$$(wildcard $$(shell $$(MISE) where lsd)/*/lsd.1),lsd))
+$(eval $(call man_file,usage,$$(shell $$(MISE) where usage)/usage.1,usage))
 
 $(eval $(call man_files,nfpm,$$(shell $$(MISE) where nfpm)/manpages,nfpm))
 
